1cacb6cdf14bf47435fbf8049e455d1f841782a8
Josep <124592854+josquefi@users.noreply.github.com>
2024-12-28 12:07:24 +0100

    Update 2_train_logit.py

--- 2_train_logit.py
+++ 2_train_logit.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Dec  6 22:05:23 2024
-
-@author: josepqueraltfibla
-"""
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
